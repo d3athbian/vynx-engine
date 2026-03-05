1ad4b82f2aef7d60469624e25a5b5fd2b5c895d7
Gabo <m4debian@gmail.com>
2026-03-04 23:38:10 -0300

    initial: scafolding

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "VynxEngine",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format === "es" ? "js" : "cjs"}`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    minify: "esbuild",
+  },
+  plugins: [
+    dts({
+      tsconfigPath: "./tsconfig.json",
+      insertTypesEntry: true,
+    }),
+  ],
+});
